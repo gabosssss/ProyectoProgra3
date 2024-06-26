ecde8a8b019e362b8f2ee273c9252420cc22e525
S-Unnamed <sanonymous9814@gmail.com>
2024-06-26 13:34:02 -0500

    Probando base de datos
    Se obtiene la instancia de Database y se hacen pruebas con las columnas.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,56 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
 #include <fstream>
 #include "Objetos/Peliculas.cpp"
 #include "Objetos/Usuario.h"
-#include "Funciones.cpp"
-
+#include "Funciones.h"
+#include "Database.h"
 using namespace std;
 
 int main() {
+    
+    Database* d = Database::getInstance(); // La base de datos con todo
+
+    //Testing
+
+    vector<string> v = d->getImdbId();
+    vector<string> v2 = d->getPlotSynopsis();
+
+    if (v.empty()) {
+        cout << "No se cargaron datos" << endl;
+    } else {
+        cout << "Datos cargados correctamente" << endl;
+    }
+
+    for (size_t i = 0; i < v.size() && i < 3; ++i) {
+        cout << v[i] << endl;
+    }
+
+    cout << v2[9];
+
+    return 0;
+}
+
+/* Código anterior 1:
+
+    ifstream data;
+    data.open("../mpst_full_data.csv");
+    string line="";
+    getline(data,line);
+    getline(data,line);
+
+    cout<<line<<endl<<endl;
+
+    vector<string> hola=split(line,',');
+    for (int i=0;i<hola.size();i++) {
+        cout<<hola[i]<<endl;
+    }
+    // Cols: imdb_id,title,plot_synopsis,tags,split,synopsis_source
+    data.close();
+*/
+/* Código anterior 2:
+
     ifstream data;
     ifstream data2;
     data.open("../mpst_full_data_fixed.csv");
@@ -28,4 +70,8 @@ int main() {
         }
         cout << endl;
     }
-}
+*/
+
+// Cosa
+// g++ main.cpp Funciones.cpp Database.cpp -o main
+// ./main.exe
