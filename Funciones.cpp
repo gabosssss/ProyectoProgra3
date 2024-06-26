567a0b57755da7fbbe61cd7d36f6d2d63f243121
S-Unnamed <sanonymous9814@gmail.com>
2024-06-26 13:32:10 -0500

    Nuevas funciones fill_database y get_col
    fill_database para obtener un vector de vectores string que almacenen todos los datos.
    get_col sirve para obtener datos de determinadas columnas y usarlo en Database.cpp

--- Funciones.cpp
+++ Funciones.cpp
@@ -1,7 +1,6 @@
+#include "Funciones.h"
+#include <fstream>
 #include <iostream>
-#include <vector>
-
-using namespace std;
 
 vector<string> split(string texto, char separador) {
     vector<string> resultado;
@@ -39,4 +38,30 @@ vector<string> split_avanzado(string texto, char separador) {
     }
 
     return resultado;
+}
+
+vector<vector<string>> fill_database() {
+    vector<vector<string>> lines;
+    ifstream data("../mpst_full_data_fixed.csv");
+    if (!data.is_open()) {
+        cerr << "Error: No se pudo abrir el archivo" << endl;
+        return lines;
+    }
+    string line = "";
+    getline(data, line);
+    while (getline(data, line)) {
+        lines.push_back(split_avanzado(line,';'));
+    }
+    data.close();
+    return lines;
+}
+
+vector<string> get_col(vector<vector<string>> database, int pos) {
+    vector<string> ret;
+    for (auto& i : database) {
+        if (pos < i.size()) {
+            ret.push_back(i[pos]);
+        }
+    }
+    return ret;
 }
